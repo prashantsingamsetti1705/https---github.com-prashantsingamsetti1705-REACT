aa105648a3f6a32c3b2f1dc24d9110caceeeb4f0
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-05-05 22:19:18 +0530

    components

---
+++ react-components/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Netflix/>
+<Netflix/>
+<Netflix/>
+    </div>
+  )
+}
+
+const Netflix=()=>{
+  return(
+    <div>
+      <div>
+        <img src="https://m.media-amazon.com/images/M/MV5BNmY4YmU2ODYtZjMxZS00ZmNhLThmODgtZWYwNTQwZDMzMzA0XkEyXkFqcGc@._V1_.jpg" height={200} width={350}alt="og" />
+      </div>
+      <h2>movie name:og movie</h2>
+      <h3>rating:5</h3>
+      <p>movie untadu chudu tamaudhu super alsa</p>
+    </div>
+  )
+}
+
+export default App
