dcb26de0944b24cfac70e605713e255cc9496707
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-05-06 20:21:26 +0530

    jsx varbile,experssion,conditional op,components,import and export

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-
-const App = () => {
+import Netflix,{Header,Footer} from './components/Netflix'
+export const App = () => {
   return (
     <div>
+      <Header/>
       <Netflix/>
 <Netflix/>
 <Netflix/>
+<Footer/>
     </div>
   )
 }
-
-const Netflix=()=>{
-  return(
-    <div>
-      <div>
-        <img src="https://m.media-amazon.com/images/M/MV5BNmY4YmU2ODYtZjMxZS00ZmNhLThmODgtZWYwNTQwZDMzMzA0XkEyXkFqcGc@._V1_.jpg" height={200} width={350}alt="og" />
-      </div>
-      <h2>movie name:og movie</h2>
-      <h3>rating:5</h3>
-      <p>movie untadu chudu tamaudhu super alsa</p>
-    </div>
-  )
-}
-
-export default App
+// it is normal form of import
+// const App = () => {
+//   return (
+//     <div>
+//       <Netflix/>
+// <Netflix/>
+// <Netflix/>
+//     </div>
+//   )
+// }
+// export default App;
---
+++ react-components/src/components/Netflix.jsx
@@ -0,0 +1,27 @@
+export const Header=()=>{
+    return(
+        <div>
+            hey hi re we almost completed the compents and import and export like default and name import 
+        </div>
+    )
+}
+const Netflix=()=>{
+    return(
+      <div>
+        <div>
+          <img src="https://m.media-amazon.com/images/M/MV5BNmY4YmU2ODYtZjMxZS00ZmNhLThmODgtZWYwNTQwZDMzMzA0XkEyXkFqcGc@._V1_.jpg" height={200} width={350}alt="og" />
+        </div>
+        <h2>movie name:og movie</h2>
+        <h3>rating:{5+3+2}</h3>
+        <p>movie untadu chudu tamaudhu super alsa</p>
+      </div>
+    )
+  } 
+export default Netflix;
+export const Footer=()=>{
+    return(
+        <div>
+            <h1>thanks for visiting this page @copyright for this page</h1>
+        </div>
+    )
+}
